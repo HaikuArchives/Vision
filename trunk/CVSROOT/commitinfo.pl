5c1aad3868836a75f19214f96c78fdaaecc35673
kurros <kurros@8ae54ada-267b-4fd0-ace1-4922a63843f8>
2001-04-13 04:06:48 +0000

    new *less annoying* email-on-checkin system

---
+++ trunk/CVSROOT/commitinfo.pl
@@ -0,0 +1,18 @@
+#! /usr/bin/perl
+# to call this script, add in you CVSROOT/commitinfo :
+# module_to_match THE_GOOF/PATH/commitinfo.pl
+
+# (c)1999  Robert CHERAMY <tibob@via.ecp.fr>
+# you are free to reuse this script
+
+$last_dir_file = "/tmp/#visioncvs.files.lastdir";
+
+$my_pgrp_id = getpgrp();
+
+$commitdir = $ARGV[0];
+
+#print "opening $last_dir_file.$my_pgrp_id\n";
+open(LDFD, ">$last_dir_file.$my_pgrp_id") ||
+  die "Cannot open file $last_dir_file.$my_pgrp_id";
+print LDFD "$commitdir\n";
+close(LDFD);
