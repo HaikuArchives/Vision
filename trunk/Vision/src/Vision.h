56ff159bb3ded705fd39a8196a2a79b0a7fd943e
anevilyak <anevilyak@8ae54ada-267b-4fd0-ace1-4922a63843f8>
2001-06-08 11:59:17 +0000

    Another minor ident fix

--- trunk/Vision/src/Vision.h
+++ trunk/Vision/src/Vision.h
@@ -42,6 +42,7 @@ class SetupWindow;
 class ClientWindow;
 class SettingsFile;
 class BLocker;
+class BNetEndpoint;
 
 extern class VisionApp * vision_app;
 
@@ -128,6 +129,7 @@ class VisionApp : public BApplication
 	BMessage                idents;
 	BLocker                 identLock;
 	thread_id               identThread;
+	BNetEndpoint            *identEndpoint;
 };
 
 const uint32 M_SETUP_CLOSE           = 'vasc';
