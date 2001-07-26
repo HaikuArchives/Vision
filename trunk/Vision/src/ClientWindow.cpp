6079dcd28fb0e1fbb2a005349b64cc59105e9ba8
anevilyak <anevilyak@8ae54ada-267b-4fd0-ace1-4922a63843f8>
2001-07-26 00:32:52 +0000

    *** empty log message ***

--- trunk/Vision/src/ClientWindow.cpp
+++ trunk/Vision/src/ClientWindow.cpp
@@ -103,8 +103,6 @@ ClientWindow::QuitRequested (void)
   }
 
 
-  vision_app->SetRect ("clientWinRect", Frame());
-  
   BMessage killMeNow (B_QUIT_REQUESTED);
   killMeNow.AddBool ("real_thing", true);  
   vision_app->PostMessage (&killMeNow);
