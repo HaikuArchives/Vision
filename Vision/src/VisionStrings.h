35e7729b1aae0b39177b1006a224810147833546
Rene Gollent <anevilyak@users.noreply.github.com>
2002-06-16 07:20:22 +0000

    * updated the update

--- Vision/src/VisionStrings.h
+++ Vision/src/VisionStrings.h
@@ -170,6 +170,7 @@
 #define S_PCMD_DNS1                 "Resolved "
 #define S_PCMD_DNS2                 " to "
 #define S_PCMD_DNS_ERROR            "Unable to resolve "
+#define S_PCMD_PEXEC_ERROR          "/pexec: command failed"
 
 // ctcp parser
 
